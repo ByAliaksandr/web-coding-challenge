1bb11ab33343a63ddec4c2137506036b1ef136de
Aliaksandr Bytsai <aliaksandr.bytsai@gmail.com>
2020-06-19 10:58:07 +0200

    feature: generate signup-form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
+enum RouterParts {
+  SignupForm = 'signup-form'
+}
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: RouterParts.SignupForm, pathMatch: 'full'},
+  { path: RouterParts.SignupForm, component: SignupFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
