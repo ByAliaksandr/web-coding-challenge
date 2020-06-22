f85ec29d8959e51bf8f65cfe3f4f11ba2b1bd677
Aliaksandr Bytsai <aliaksandr.bytsai@gmail.com>
2020-06-22 10:19:47 +0200

    feature: add signupService unit tests

--- src/app/signup-form/service/signup.service.spec.ts
+++ src/app/signup-form/service/signup.service.spec.ts
@@ -1,16 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-
 import { SignupService } from './signup.service';
+import { HttpClient } from '@angular/common/http';
+import { SingupRequest } from './signup-request.interface';
 
 describe('SignupService', () => {
-  let service: SignupService;
+  let signupService: SignupService;
+  let httpSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SignupService);
+    httpSpy = jasmine.createSpyObj('http', ['post']);
+    signupService = new SignupService(httpSpy);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(signupService).toBeTruthy();
+  });
+
+  describe('signup', () => {
+    it('should be make a demo call', () => {
+      const signupData: SingupRequest = {
+        firstName: 'Aaa',
+        lastName: 'Bbb',
+        email: 'aa.bb@gmail.com',
+        password: 'Password',
+      };
+
+      signupService.signup(signupData);
+
+      expect(httpSpy.post).toHaveBeenCalledWith('https://demo-api.now.sh/users', signupData);
+    });
   });
 });
